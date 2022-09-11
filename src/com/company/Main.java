5b74cf80f94c0e8ad46f08d006ab0e4c457e7328
suban <nursuluu@gmx.de>
2022-09-11 18:07:17 +0200

    Birinchi Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-         
+
+        Student student = new Student("Ali", 23);
+        System.out.println(student);
     }
 }
